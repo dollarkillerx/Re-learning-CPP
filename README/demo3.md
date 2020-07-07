b51cd1c1c911a1d20ed47bcc8cb51ff36c87e79f
dollarkillerx <adapawang@gmail.com>
2020-07-07 08:58:00 +0800

    [UPD] Readme

---
+++ README/demo3.md
@@ -0,0 +1,16 @@
+### VS 常用快捷键
+- 调试
+    - F9: 切换断点
+    - Ctrl + F5 开始执行(不调试)
+    - F5 开始调式，next(跳过一个断点)
+    - Shift + F5 停止调式
+    - Ctrl + Shift + F5 重启调试
+    - F10 step over
+    - F11 step into
+    - Shift + F11: step out
+- Ctrl + J智能提示
+- Tab 选择贺使用智能提示内容
+
+- 组合键
+    - Ctrl + M，O 折叠所有方法
+    - Ctrl + M, L 展开所有方法
---
+++ demo/demo/demo1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std; // 引入命名空间
+
+void test1();
+void test2();
+int add(int a, int b);
+double add(double a, double b);
+
+int main()
+{
+	test1();
+	test2();
+	return 0;
+}
+
+void test2()
+{
+	cout << "1 + 1 = " << add(1, 1) << endl;
+	cout << "1 + 2.6 = "<< add(1.0,2.6) << endl;
+	// 函数重载使用 name margling 技术  编译时需要函数重载的函数名称会发生改变
+	// 举一个栗子 (不同的编译器生成规则不一样)
+	// add(int a ,int b) => add_int(int a,int b) 
+	// add(double a,double b) => add_double(double a,double b)
+}
+
+int add (int a,int b)
+{
+	return a + b;
+}
+double add (double a,double b)
+{
+	return a + b;
+}
+
+void test1()
+{
+	cout << "Hello World" << endl << "Fuck you" << endl; // 好像管道
+
+	int age;
+	cin >> age;
+	if (age == 0)
+	{
+		cout << "you not in number" << endl;
+	}
+	else
+	{
+		cout << "you in: " << age << endl;
+	}
+
+	// cout, console out
+	// cin, console in
+	// << 左移运算符
+}
+
